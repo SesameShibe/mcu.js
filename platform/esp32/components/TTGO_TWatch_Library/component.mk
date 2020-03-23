eb88dbf3c209c6188c1680b5a51d3a356870e242
LITTOMA <littoma@hotmail.com>
2020-03-23 21:39:57 +0800

    Switch to TTGO T-Watch library.

---
+++ platform/esp32/components/TTGO_TWatch_Library/component.mk
@@ -0,0 +1,6 @@
+COMPONENT_NAME := TTGO_TWatch
+COMPONENT_ADD_INCLUDEDIRS := src
+COMPONENT_SRCDIRS := src src/Button2/src src/drive/axp \
+src/drive/bma423 src/drive/fx50xx src/drive/i2c src/drive/rtc src/drive/tft \
+src/font src/imgs src/lvgl src/PN532 \
+src/S7xG_Library/src src/TFT_eSPI
--- platform/esp32/main/component.mk
+++ platform/esp32/main/component.mk
@@ -2,6 +2,6 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-COMPONENT_SRCDIRS := . __generated TFT_eSPI
+COMPONENT_SRCDIRS := . __generated
 COMPONENT_EXTRA_INCLUDES := ../../../../duktape
-COMPONENT_EXTRA_INCLUDES += ../../main ../../main/TFT_eSPI
+COMPONENT_EXTRA_INCLUDES += ../../main
