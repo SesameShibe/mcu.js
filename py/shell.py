c7773cf826c630ae985cd1a36cfe694fb046e9ad
LITTOMA <littoma@hotmail.com>
2020-05-20 22:46:39 +0800

    Fix file encoding problem

--- py/shell.py
+++ py/shell.py
@@ -8,10 +8,25 @@ import threading
 from prompt_toolkit import PromptSession
 from prompt_toolkit.history import FileHistory
 
-from font import getFileEncoding
-
 COM_PORT = 'COM6'
 ser = ''
+
+def getFileEncoding(path):
+    with open(path, 'rb')as f:
+        magic = f.read(4)
+        # utf-8
+        if(magic[:3] == '\xef\xbb\xbf'):
+            return 'utf-8-sig'
+        elif (magic[:2] == '\xff\xfe'):
+            return 'utf-16le'
+        elif (magic[:2] == '\xfe\xff'):
+            return 'utf-16be'
+        elif (magic == '\x00\x00\xff\xfe'):
+            return 'utf-32le'
+        elif (magic == '\xfe\xff\x00\x00'):
+            return 'utf-32be'
+        else:
+            return 'utf-8'
 
 def recvLoop():
     reader = codecs.getreader('utf-8')(ser)
